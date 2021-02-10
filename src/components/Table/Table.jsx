3e9a72aa91b7f7ebdf0a64f9573744b1da602349
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-02-09 23:11:40 -0500

    users can be displayed in ascending or desc order

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -3,13 +3,60 @@ import axios from "axios";
 
 const Table = () => {
   const [users, setUsers] = useState([]);
+  const [usersToDisplay, setUsersToDisplay] = useState([]);
+  const [sortDirection, setSortDirection] = useState("asc");
 
   useEffect(() => {
       axios.get("https://randomuser.me/api/?results=50").then(response => {
           console.log(response.data);
+          setUsersToDisplay(response.data.results);
           setUsers(response.data.results);
       })
   }, []);
+
+  const sortByName = () => {
+      if(sortDirection === "asc"){
+          sortByNameAsc();
+          setSortDirection("desc");
+      } else {
+          sortByNameDesc();
+          setSortDirection("asc");
+      }
+  };
+
+  const sortByNameAsc = () => {
+    const tempUsers = [...users];
+    const sortedUsers = tempUsers.sort((a,b) => {
+        const aValue = a.name.first;
+        const bValue = b.name.first;
+        if (aValue < bValue) {
+            return -1;
+        }
+        if (aValue > bValue) {
+            return 1;
+        }
+        return 0;
+    });
+    console.log(sortedUsers);
+    setUsersToDisplay(sortedUsers);
+  };
+
+  const sortByNameDesc = () => {
+    const tempUsers = [...users];
+    const sortedUsers = tempUsers.sort((a,b) => {
+        const aValue = a.name.first;
+        const bValue = b.name.first;
+        if (aValue > bValue) {
+            return -1;
+        }
+        if (aValue < bValue) {
+            return 1;
+        }
+        return 0;
+    });
+    console.log(sortedUsers);
+    setUsersToDisplay(sortedUsers);
+  };
 
   return (
     <table className="table table-striped">
@@ -17,15 +64,15 @@ const Table = () => {
         <tr>
           <th scope="col">ID</th>
           <th scope="col">Photo</th>
-          <th scope="col">Name</th>
+          <th scope="col" onClick={sortByName}>Name</th>
           <th scope="col">Email</th>
           <th scope="col">Phone</th>
           <th scope="col">DOB</th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user) => (
-          <tr>
+        {usersToDisplay.map((user, index) => (
+          <tr> key={index}
             <th scope="row">{user.id.value}</th>
             <td>
               <img src={user.picture.thumbnail}></img>
@@ -35,7 +82,7 @@ const Table = () => {
             <td>{user.phone}</td>
         <td>{user.dob.date}</td>
           </tr>
-        ))}
+        ))};
       </tbody>
     </table>
   );
